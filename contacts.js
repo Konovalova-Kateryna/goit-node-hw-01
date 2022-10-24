f24a23007aa7db9e27ffc8ab3cfbcf3bbe9cc3ab
Konovalova-Kateryna <kytana2112@gmail.com>
2022-10-24 09:50:16 +0300

    add try/catch

--- contacts.js
+++ contacts.js
@@ -3,43 +3,63 @@ const path = require("path");
 const { nanoid } = require("nanoid");
 
 const contactsPath = path.join(__dirname, "./db/contacts.json");
-// console.log(contactsPath);
 
 async function getDb() {
-  const contacts = await fs.readFile(contactsPath);
-  const contactsList = JSON.parse(contacts);
-  return contactsList;
+  try {
+    const contacts = await fs.readFile(contactsPath, { encoding: "utf8" });
+    const contactsList = JSON.parse(contacts);
+    return contactsList;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 async function listContacts() {
-  const contactsList = await getDb();
-  return contactsList;
+  try {
+    const contactsList = await getDb();
+    return contactsList;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 async function getContactById(id) {
-  const contactList = await getDb();
-  const contact = contactList.find((item) => item.id === id);
-  return contact;
+  try {
+    const contactList = await getDb();
+    const contact = contactList.find((item) => item.id === id);
+    return contact;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 async function removeContact(contactId) {
-  const contactsList = await getDb();
-  const contactItem = contactsList.find((item) => item.id === contactId);
-  if (!contactItem) {
-    return null;
+  try {
+    const contactsList = await getDb();
+    const contactItem = contactsList.find((item) => item.id === contactId);
+    if (!contactItem) {
+      console.warn("Unknown id");
+      return null;
+    }
+    const contacts = contactsList.filter((item) => item.id !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return;
+  } catch (error) {
+    console.error(error.message);
   }
-  const contacts = contactsList.filter((item) => item.id !== contactId);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  return contactItem;
 }
 
 async function addContact(name, email, phone) {
-  const id = nanoid();
-  const contactItem = { id, name, email, phone };
-  const contactsList = await getDb();
-  contactsList.push(contactItem);
-  await fs.writeFile(contactsPath, JSON.stringify(contactsList));
-  return contactItem;
+  try {
+    const id = nanoid();
+    const contactItem = { id, name, email, phone };
+    const contactsList = await getDb();
+    contactsList.push(contactItem);
+    await fs.writeFile(contactsPath, JSON.stringify(contactsList));
+    return;
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 module.exports = {
